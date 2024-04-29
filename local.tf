4a63685f048a08fe2e06b5fd85a68677854aaa20
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-04-28 20:56:31 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,31 +1,26 @@
 locals{
   landmarks1="museum"
- 
+ landmarks2="Notre-Dame Basilica"
+landmarks3="Biodome"
+landmarks4="Habitat 67"
+landmarks5="Mount Royal Park"
 }
 output "print1"{
     value = local.landmarks1
 }
-landmarks2="Notre-Dame Basilica"
  
-}
 output "print2"{
     value = local.landmarks2
 }
-landmarks3="Biodome"
- 
-}
+
 output "print3"{
     value = local.landmarks3
 }
-landmarks4="Habitat 67"
- 
-}
+
 output "print4"{
     value = local.landmarks4
 }
-landmarks5="Mount Royal Park"
- 
-}
+
 output "print5"{
     value = local.landmarks5
 }
