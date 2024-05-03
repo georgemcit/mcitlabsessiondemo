0f060d0cc82943b5d71165a771cf07c28b4ff23b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-02 21:35:28 -0400

    Update variable output.tf

--- variable output.tf
+++ variable output.tf
@@ -7,4 +7,7 @@ output "uppercase_string" {
 output "lowercase_string" {
   value = lower(var.original_string)
 }
+output "concatenated_string" {
+  value = "${var.original_string}${var.additional_string}"
+}
 
