d3e08a34740600e6cf715564bb8bdcfd7e9a5188
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-03 18:54:36 -0400

    Update George variable.tf

--- George variable.tf
+++ George variable.tf
@@ -29,3 +29,18 @@ variable "george_list" {
 variable "additional1_string" {
   default = " from syria"
 }
+variable "simple_nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+variable "nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
+}
