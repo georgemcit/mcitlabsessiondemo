8950983395ad524241fe8e0344b8fbefbccc050f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-03 19:08:23 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,4 +21,5 @@ variable "simple_nested_list" {
   type    = list(list(string))
   default = [["a", "b"], ["c", "d"], ["e", "f"]]
 }
-simple_local_flattened_list = flatten(var.simple_nested_list)
+simple_local_flattened_list = flatten(var.simple_nested_list)\
+}
