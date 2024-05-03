7a071a2149e62bd15217e1747ee98c21515f6749
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-03 19:22:25 -0400

    Update map variable.tf

--- map variable.tf
+++ map variable.tf
@@ -2,9 +2,16 @@ variable "simple_nested_list" {
   type    = list(list(string))
    default = [["a", "b"], ["c", "d"], ["e", "f"]]
  }
+
 locals{
    simple_local_flattened_list = flatten(var.simple_nested_list)
 }
+locals{
+   flattened_list = flatten(var.nested_list)
+}
 output "simple_local"{
+    value = local.simple_local_flattened_list
+}
+output "flattened_list"{
     value = local.simple_local_flattened_list
 }
