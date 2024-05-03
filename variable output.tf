e2e83609e103138993d650c258d17cf4316cbbf5
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-02 21:23:03 -0400

    Update variable output.tf

--- variable output.tf
+++ variable output.tf
@@ -1,4 +1,3 @@
-
 output "string_length" {
   value = length(var.original_string)
 }
@@ -8,8 +7,17 @@ output "uppercase_string" {
 output "lowercase_string" {
   value = lower(var.original_string)
 }
+output "replaced_string" {
+  value = replace(var.original_string, "IBRAHIM", "GEORGE")
+}
 output "join_name" {
   value = join(" ", var.string_list)
+}
+output "concatenated_string" {
+  value = "${var.original_string}${var.additional_string}"
+}
+output "substring_example" {
+  value = substr(var.original_string, 3, -1)
 }
 output "trimmed_string" {
   value = trim(var.nospace_string," ")
@@ -21,12 +29,12 @@ output "reverse_example" {
   value = join("", reverse(split("", var.original_string)))
 }
 locals {
-  contains_keyword = contains(split(" ", var.original_string), "CANADA")
+  contains_keyword = contains(split(" ", var.original_string), "GEORGE")
 }
 output "contains"{
     value = local.contains_keyword
 
 }
 output "string_contains_no_sample" {
-  value = local.contains_keyword ? "String contains 'CANADA'" : "String does not contain 'CANADA'"
+  value = local.contains_keyword ? "String contains 'GEORGE'" : "String does not contain 'IBRAHIM'"
 }
