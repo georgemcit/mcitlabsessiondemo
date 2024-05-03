c4fa3cde129c3dc02320d7ffca86006ad261dd35
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-02 20:18:12 -0400

    Update local output.tf

--- local output.tf
+++ local output.tf
@@ -1,24 +1,18 @@
-}
 output "print1"{
     value = local.landmarks1
 }
- 
-output "print2"{
+ output "print2"{
     value = local.landmarks2
 }
-
 output "print3"{
     value = local.landmarks3
 }
-
 output "print4"{
     value = local.landmarks4
 }
-
 output "print5"{
     value = local.landmarks5
 }
-
 output "company_name"{
  value=var.company_name
 }
