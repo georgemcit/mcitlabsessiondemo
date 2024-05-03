639921552aa098db3e3117d92a50c83d108b847a
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-03 19:25:39 -0400

    Update map variable.tf

--- map variable.tf
+++ map variable.tf
@@ -2,6 +2,18 @@ variable "simple_nested_list" {
   type    = list(list(string))
    default = [["a", "b"], ["c", "d"], ["e", "f"]]
  }
+variable "nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
+}
 
 locals{
    simple_local_flattened_list = flatten(var.simple_nested_list)
