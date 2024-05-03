1b0357c95858df285b4cc2017e96e2d873032f2b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-02 20:25:25 -0400

    Update George variable.tf

--- George variable.tf
+++ George variable.tf
@@ -1,1 +1,12 @@
-
+variable "original_string" {
+  default = "GEORGE IBRAHIM"
+}
+variable "string_list" {
+  default = ["GEORGE", "IBRAHIM"]
+}
+variable "additional_string" {
+  default = " from syria"
+}
+variable "nospace_string" {
+  default = "  Today is Monady "
+}
