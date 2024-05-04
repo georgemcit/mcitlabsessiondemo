a5b13faf7d16af04b9af783d6c5072cf575ee992
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-03 20:42:06 -0400

    Update map variable.tf

--- map variable.tf
+++ map variable.tf
@@ -25,15 +25,14 @@ variable "nested_map" {
 
 locals{
    simple_local_flattened_list = flatten(var.simple_nested_list)
+   number_list = flatten(var.number_list)
    flattened_list = flatten(var.nested_list)
    flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
    characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
   character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed[index]
-  number_list = flatten(var.number_list)
   }
-
 }
 output "simple_local"{
     value = local.simple_local_flattened_list
