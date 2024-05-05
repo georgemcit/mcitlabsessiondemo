0b470812fe23de58b774c1da3e5b3cae89da82a3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-04 20:59:00 -0400

    Update map variable.tf

--- map variable.tf
+++ map variable.tf
@@ -56,3 +56,16 @@ output "characters"{
 output "number_list"{
     value = local.number_list
 }
+variable "total_output" {
+  type    = list(string)
+  default = ["150", "150", "150"]
+}
+
+locals {
+  incremented_output = [
+    for value in var.total_output : tostring(tonumber(value) + 10)
+  ]
+}
+output "incremented_output_show" {
+  value=local.incremented_output
+}
