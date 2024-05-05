a617479bbcb8f701fdcc69b225920991f0f43d0b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-04 20:55:16 -0400

    Update variable output.tf

--- variable output.tf
+++ variable output.tf
@@ -34,6 +34,3 @@ output "visitcountries"{
 output "join_george" {
   value = join(" ", var.george_list)
 }
-output "incremented_output"{
-    value=sum([for numberoutput in var.incremented_output: tonumber(numberoutput)])
-}
