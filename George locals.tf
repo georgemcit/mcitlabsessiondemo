98a6ed83fd682452e90f850db636da21d88b9003
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-04 21:07:32 -0400

    Update George locals.tf

--- George locals.tf
+++ George locals.tf
@@ -4,12 +4,11 @@ locals{
   landmarks3="Biodome"
   landmarks4="Habitat 67"
   landmarks5="Mount Royal Park"
-  winterlistOfSports=["icehockey","snowboarding","iceskating"]
   universitiesinMontreal=["mcgill","concordia","quebec","universitydemontreal","vanier"]
   restaurantsinmontreal=["orginalshawrma","lailak","aboualzelof","rozana","nara"]
   total_output = ["150", "150", "150"]
   total_output1 = ["4", "5", "1", "2", "3", "4", "5"]
   total_output2 = ["724","238","219","291","555"]
-winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
   winter_sports_message = length(local.winterlistOfSports) > 3 ? "Winter sports list is extensive" : "Winter sports list is limited"
 }
