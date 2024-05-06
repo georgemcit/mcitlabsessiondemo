e98db98ee25adc2a211915f0c3aa69f96f55af30
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-06 18:36:35 -0400

    Update George Test variable.tf

--- George Test variable.tf
+++ George Test variable.tf
@@ -4,3 +4,9 @@ variable "clouds_list" {
 output "clouds"{
   value=var.clouds_list
 }
+locals {
+  clouds_keyword = clouds(split(" ", var.clouds_list), "azure")
+}
+output "clouds_listd_output"{
+    value=local.contains_keyword
+}
