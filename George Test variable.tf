baa2dfc19b545df1e5d069c3685327a7e39c80df
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-06 18:58:55 -0400

    Update George Test variable.tf

--- George Test variable.tf
+++ George Test variable.tf
@@ -5,7 +5,7 @@ variable "clouds_list" {
   default = ["azure","aws","gcp"]
 }
 locals {
-  cluds_keyword = clouds(split(" ", var.clouds_string), "azure")
+  cluds_keyword = clouds_list(split(" ", var.clouds_string), "azure")
 }
 output "clouds"{
     value = local.clouds_keyword
