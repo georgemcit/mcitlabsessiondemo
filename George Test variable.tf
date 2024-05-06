298336c4fb84a934b139acb14c5dcdc227a8c383
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-06 18:51:23 -0400

    Update George Test variable.tf

--- George Test variable.tf
+++ George Test variable.tf
@@ -1,15 +1,14 @@
 variable "clouds_list" {
   default = ["azure","aws","gcp"]
 }
-output "clouds_list"{
+output "clouds"{
   value=var.clouds_list
 }
 locals {
   clouds_keyword = clouds(split(" ", var.clouds_list), "azure")
 }
 output "clouds"{
-    value = local.clouds_list_keyword
+    value = local.clouds_keyword
 }
-output "string_clouds_list" {
-  value = local.cloudss_keyword ? "String clouds 'azure'" : "String does not contain 'azure'"
-}
+output "string_clouds" {
+  value = local.clouds_keyword ? "String clouds 'azure'" : "String does not contain 'azure'"
