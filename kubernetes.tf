78d39028c259dc0a35bc6b1c74a7cb83e86ef47f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-06 20:21:10 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -5,10 +5,3 @@ resource "azurerm_resource_group" "georgeazureresourcegroup" {
   name     = "George_ibrahim_1980"
   location = "Canada Central"
 }
-locals{ 
-  cluster_names_1=["georgeibrahimazureresourcegroup"]
-}
-resource "azurerm_resource_group" "georgeibrahimazureresourcegroup" {
-  name     = "George_ibrahim_05_1980"
-  location = "East US"
-}
