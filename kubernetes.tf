7c6e00c5bf57d3f246993d4840695ef3bd789899
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-06 20:52:43 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,23 +1,32 @@
 locals{ 
-  cluster_names=["georgeazureresourcegroup"]
+  cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
 resource "azurerm_resource_group" "georgeazureresourcegroup" {
-  resource =var.azurerm_resource_group
-  name =var.George_ibrahim_05_1980
-  location =var.UAE North 
+  name     = "george_ibrahim_05_1980"
+  location = "Canada Central"
 }
-variable "resource"{
-  type=string+
+resource "azurerm_resource_group" "georgeazureresourcegroup" {
+  name     = "george_ibrahim_01_05_1980"
+  location = "East US"
 }
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
 
-variable "resource"{
-  type=string
-}
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
 
-variable "name"{
-  type=string
-}
+  identity {
+    type = "SystemAssigned"
+  }
 
-variable "location"{
-  type=string
+  tags = {
+    Environment = "Production"
+  }
 }
