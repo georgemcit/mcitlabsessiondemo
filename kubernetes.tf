448c6343d0f93e35d17c87c9db6d59ca0e49743d
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-07 23:00:35 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,7 @@
+resource "azurerm_resource_group"{
+  name     = "george_ibrahim_resource_group"
+  location = "West Europe"
+}
 locals{ 
   cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
