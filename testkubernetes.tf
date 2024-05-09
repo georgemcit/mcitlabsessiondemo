83bad17e639d94e04c35b313408adcc772ec7655
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-08 20:34:36 -0400

    Update testkubernetes.tf

--- testkubernetes.tf
+++ testkubernetes.tf
@@ -61,7 +61,7 @@ output "tags" {
   value = var.tags
 }
 resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
-for_each               = azurerm_kubernetes_cluster.batchabcd
+for_each               = azurerm_kubernetes_cluster.george
 name                   = "${each.key}"
 kubernetes_cluster_id  = each.value.id
 vm_size                = "Standard_DS2_v2"
