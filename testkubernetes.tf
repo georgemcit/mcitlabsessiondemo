a7cad5f81aba87e5575c423a267b7da59c09c8a8
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-08 20:42:29 -0400

    Update testkubernetes.tf

--- testkubernetes.tf
+++ testkubernetes.tf
@@ -1,6 +1,6 @@
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{
-   cluster_list=["ccrf2301_montrealcluster","ccrf2301_torontocluster","ccrf2301_vancouvercluster","ccrf2301_albertacluster"]
+   cluster_list=["montreal","toronto","vancouver","alberta"]
 }
 resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
   name                = "georgeibrahimcluster"
