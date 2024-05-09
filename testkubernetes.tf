1db474c6a76f2050ea8c1729709977aea19d620b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-08 20:46:18 -0400

    Update testkubernetes.tf

--- testkubernetes.tf
+++ testkubernetes.tf
@@ -72,41 +72,41 @@ node_count             = 1
 }
 output "id" {
   value = [
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.id
+    for cluster in azurerm_kubernetes_cluster.george: cluster.id
   ]
 }
  
 output "kube_config" {
   sensitive = true
   value = [ 
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw
+    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config_raw
   ]
 }
  
 output "client_key" {
   sensitive = true
   value = [
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.client_key
+    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_key
   ]
 }
  
 output "client_certificate" {
   sensitive = true
   value = [
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.client_certificate
+    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_certificate
   ]
 }
  
 output "cluster_ca_certificate" {
   sensitive = true
   value = [
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.cluster_ca_certificate
+    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.cluster_ca_certificate
   ]
 }
  
 output "host" {
   sensitive = true
   value = [
-    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.host
+    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.host
   ]
 }
