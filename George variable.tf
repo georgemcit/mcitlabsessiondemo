5640cbaa8d06b248c9083341db266f81fca44a28
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 20:56:46 -0400

    Update George variable.tf

--- George variable.tf
+++ George variable.tf
@@ -65,3 +65,74 @@ output "visitcountries"{
 output "join_george" {
   value = join(" ", var.george_list)
 }
+variable "company_name"{
+ type=string
+ default="Ipex"
+}
+variable "years_of_work"{
+ type=number
+ default=2
+}
+variable "ifstillworking"{
+ type=bool
+ default=true
+}
+variable "subscription_id"{
+  type=string
+}
+
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "prefix"{
+ type=string
+ default="samiislam"
+}
+variable "firstname"{
+ type=string
+ default="sami"
+}
+variable "lastname"{
+ type=string
+ default="islam"
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+variable "environment"{
+ type=string
+ default="staging"
+}
+variable "summersports"{
+    type =list(string)
+    default=["soccer","basketball","badminton","tabletennis"]
+}
+output "summersportslist"{
+    value=[for summersport in var.summersports:summersport ]
+}
+variable "favouritefood"{
+    type =list(string)
+    default=["tabola","mlokia","shakrea","shawrma","falafel"]
+}
+output "favouritefood"{
+    value=[for favouritefood in var.favouritefood:favouritefood ]
+}
