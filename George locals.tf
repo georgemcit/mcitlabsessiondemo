5d838c2057342036df7b9e44b4ca9d560196d763
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 20:48:21 -0400

    Update George locals.tf

--- George locals.tf
+++ George locals.tf
@@ -12,3 +12,66 @@ locals{
   winter_sports_message = length(local.winterlistOfSports) > 3 ? "Winter sports list is extensive" : "Winter sports list is limited"
   winterlistOfSports=["icehockey","snowboarding","iceskating"]
 }
+output "print1"{
+    value = local.landmarks1
+}
+ output "print2"{
+    value = local.landmarks2
+}
+output "print3"{
+    value = local.landmarks3
+}
+output "print4"{
+    value = local.landmarks4
+}
+output "print5"{
+    value = local.landmarks5
+}
+output "company_name"{
+ value=var.company_name
+}
+output "ifstillworking"{
+ value=var.ifstillworking
+}
+output "years_of_work"{
+ value=var.years_of_work
+}
+output "wintersportslist"{
+     value =[for sport in local.winterlistOfSports:sport]
+}
+output "universitiesinMontreal"{
+     value =[for university in local.universitiesinMontreal: university]
+}
+output "restaurantsinmontreal"{
+     value =[for restaurant in local.restaurantsinmontreal:restaurant]
+}
+output "sumofthreenumbers"{
+    value=sum([for numberoutput in local.total_output: tonumber(numberoutput)])
+}
+output "sumofsevennumbers"{
+    value=sum([for numberoutput in local.total_output1: tonumber(numberoutput)])
+}
+output "print"{
+    value = local.servicename
+}
+output "secondprint"{
+    value = local.forum
+}
+output "lengthsa"{
+    value=local.lengthsa
+}
+output "lengthforum"{
+    value=local.lengthforum
+}
+output "thirdprint"{
+    value=local.firstname
+}
+output "fourthprint"{
+    value=local.city
+}
+output "sumotest"{
+    value=sum([for numberoutput in local.total_output2: tonumber(numberoutput)])
+}
+output "winter_sports_message" {
+  value = local.winter_sports_message
+}
