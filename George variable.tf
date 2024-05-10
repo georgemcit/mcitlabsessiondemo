d14b096c8fec2e77e41a0491310dd3942eb24f43
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 20:50:14 -0400

    Update George variable.tf

--- George variable.tf
+++ George variable.tf
@@ -29,4 +29,39 @@ variable "george_list" {
 variable "additional1_string" {
   default = " from syria"
 }
-
+output "string_length" {
+  value = length(var.original_string)
+}
+output "uppercase_string" {
+  value = upper(var.original_string)
+}
+output "lowercase_string" {
+  value = lower(var.original_string)
+}
+output "concatenated_string" {
+  value = "${var.additional_string}${var.original_string}"
+}
+output "reverse_examplesecond"{
+  value=reverse(var.string_list)
+}
+locals {
+  contains_keyword = contains(split(" ", var.original_string), "CANADA")
+}
+output "contains"{
+    value = local.contains_keyword
+}
+output "string_contains_no_sample" {
+  value = local.contains_keyword ? "String contains 'CANADA'" : "String does not contain 'CANADA'"
+}
+output "concatenate_string" {
+  value = "${var.love_string}${var.space_string}${var.canada_string}"
+}
+output "join_example" {
+  value = join("+", var.string_list)
+}
+output "visitcountries"{
+    value=[for visitcountries in var.visitcountries:visitcountries ]
+}
+output "join_george" {
+  value = join(" ", var.george_list)
+}
