790a57b77cdd6623372c3d90282568596ef80c67
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-13 19:00:57 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -12,17 +12,17 @@ resource "azurerm_kubernetes_cluster" "simplekubernetescluster1" {
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   dns_prefix          = "exampleaks1"
 default_node_pool {
-    name       = var.default
-    node_count = var.number
-    vm_size    = var.Standard_D2_v2
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
-    type = var.SystemAssigned
+    type = "SystemAssigned"
   }
 
   tags = {
-    Environment = var.Production
+    Environment = "Production"
   }
 }
 resource "azurerm_kubernetes_cluster" "simplekubernetescluster2" {
