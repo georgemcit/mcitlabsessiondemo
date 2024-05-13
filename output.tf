8494cd631e31088d55200571c5e393145ed27f7e
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-13 19:25:34 -0400

    Update and rename george.tf to output.tf

---
+++ output.tf
@@ -0,0 +1,18 @@
+varible "dns_prefix"{
+  type=string
+{
+varible "default_node_pool_name"{
+  type=string
+{
+varible "default_node_pool_count"{
+  type=number
+{
+varible "identity"{
+  type=string
+{
+varible "environment_tag"{
+  type=string
+{
+varible "default_node_pool_vm_size"{
+  type=number
+{
