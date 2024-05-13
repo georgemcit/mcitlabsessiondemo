ed9c9021d9d55ade423b9b0105d774254bbb46e2
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-13 19:34:02 -0400

    Update testkubernetes.tf

--- testkubernetes.tf
+++ testkubernetes.tf
@@ -22,6 +22,27 @@ resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
     Environment = "Production"
   }
 }
+resource "azurerm_kubernetes_cluster" "george" {
+  for_each            = {for cluster in local.cluster_list: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "ccrf2301"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
 output "kube_name"{
   value=[for cluster in azurerm_kubernetes_cluster.george:cluster.name ]
 }
