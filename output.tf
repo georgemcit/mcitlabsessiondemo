72820c54c2304097286827b7dde5e5e320d3b2da
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-13 19:32:45 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,18 +1,1 @@
-variable "dns_prefix"{
-  type=string
-}
-variable  "default_node_pool_name"{
-  type=string
-}
-variable  "default_node_pool_count"{
-  type=number
-}
-variable  "identity1"{
-  type=string
-}
-variable  "environment_tag"{
-  type=string
-}
-variable  "default_node_pool_vm_size"{
-  type=number
-}
+
