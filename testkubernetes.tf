bc65037f0c5c3aadafec0e0cd454f952374df6ac
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 22:08:50 -0400

    Update testkubernetes.tf

--- testkubernetes.tf
+++ testkubernetes.tf
@@ -1,3 +1,4 @@
+*/
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{
    cluster_list=["montreal","toronto","vancouver","alberta"]
@@ -110,3 +111,4 @@ output "host" {
     for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.host
   ]
 }
+/*
