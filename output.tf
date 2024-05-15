950ac0f2c4c3d2181340e5bbf52a05983f1b1ab3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 23:21:33 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -19,3 +19,28 @@ resource "azurerm_kubernetes_cluster" "george1" {
     Environment = var.environment_tag
   }
 }
+
+variable "dns_prefix"{
+  type=string
+  default="george"
+}
+variable  "default_node_pool_name"{
+  type=string
+  default="ibrahim"
+}
+variable  "default_node_pool__node_count"{
+  type=number
+  default=1
+}
+variable  "identity"{
+  type=string
+  default="SystemAssigned"
+}
+variable  "environment_tag"{
+  type=string
+  default="Production"
+}
+variable  "default_node_pool_vm_size"{
+  type=string
+  default="Standard_D2_v2"
+}
