5f3d653c3af88a07d98533cd572519fba9ae86e4
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 22:11:41 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 resource "azurerm_kubernetes_cluster" "george1" {
-  for_each            = {for cluster in local.cluster_list: cluster=>cluster}
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
   name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
