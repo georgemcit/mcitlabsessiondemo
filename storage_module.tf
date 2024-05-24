5ee533e2940bc254973256a72c959caed75f6309
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-24 19:14:24 -0400

    Update storage_module.tf

--- storage_module.tf
+++ storage_module.tf
@@ -14,6 +14,3 @@ output "storage_account_name" {
 output "storage_account_id" {
   value = module.storage_account.storage_account_id
 }
-output "resource_group_name" {
-  value = module.resource_group.azurerm_resource.name
-}
