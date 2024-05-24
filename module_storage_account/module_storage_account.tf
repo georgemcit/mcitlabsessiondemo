2f8d8aba390cc8f989cb72f39304309abcab001f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-24 19:21:02 -0400

    Update module_storage_account.tf

--- module_storage_account/module_storage_account.tf
+++ module_storage_account/module_storage_account.tf
@@ -35,10 +35,3 @@ resource "azurerm_storage_account" "storage_account" {
 
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage_account.name
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.storage_account.id
-}
