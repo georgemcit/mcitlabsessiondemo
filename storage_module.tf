b390874673b01a841f470c5a296d3d0e87620e52
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-24 19:19:57 -0400

    Update storage_module.tf

--- storage_module.tf
+++ storage_module.tf
@@ -7,10 +7,3 @@ module "storage_account" {
   account_tier          = "Standard"
 }
 
-output "storage_account_name" {
-  value = module.storage_account.storage_account_name
-}
-
-output "storage_account_id" {
-  value = module.storage_account.storage_account_id
-}
