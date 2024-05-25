19324077e562659986faa0d740b451b7103e6b45
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-24 20:23:20 -0400

    Update network.tf

--- network/network.tf
+++ network/network.tf
@@ -1,8 +1,1 @@
-data "azurerm_virtual_network" "example" {
-  name                = "production"
-  resource_group_name = "networking"
-}
 
-output "virtual_network_id" {
-  value = data.azurerm_virtual_network.example.id
-}
