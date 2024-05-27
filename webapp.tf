33f3c771868050210a7cdf474f406a618b361dd8
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 19:29:33 -0400

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -11,6 +11,12 @@ resource "azurerm_subnet" "app" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.address_prefixes_app
 }
+resource "azurerm_subnet" "db" {
+  name                 = var.azurerm_subnet_db_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.address_prefixes_db
+}
 variable "azurerm_subnet_web_name"{
   type=string
 }
@@ -21,5 +27,11 @@ variable "azurerm_subnet_app_name"{
   type=string
 }
 variable "address_prefixes_app"{
+  type =list
+}
+variable "azurerm_subnet_db_name"{
+  type=string
+}
+variable "address_prefixes_db"{
   type =list
 }
