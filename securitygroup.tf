0c8da0edc2bcdd5499ebc2603e96816a045d5bb6
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 19:47:41 -0400

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,26 +1,26 @@
 resource "azurerm_network_security_group" "web_nsg" {
-  name                = var.azurerm_network_security_group_web
+  name                = var.azurerm_network_security_group_web_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_group" "app_nsg" {
-  name                = var.azurerm_network_security_group_app
+  name                = var.azurerm_network_security_group_app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_group" "db_nsg" {
-  name                = var.azurerm_network_security_group_db
+  name                = var.azurerm_network_security_group_db_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-variable "azurerm_network_security_group_web"{
+variable "azurerm_network_security_group_web_name"{
   type=string
 }
-variable "azurerm_network_security_group_app"{
+variable "azurerm_network_security_group_app_name"{
   type=string
 }
-variable "azurerm_network_security_group_db"{
+variable "azurerm_network_security_group_db_name"{
   type=string
 }
