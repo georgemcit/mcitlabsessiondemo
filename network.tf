c60843ad4d17f6371de14a940a5c4c861a8b9f12
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 19:08:15 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg" {
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myVNet"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.virtual_network_name
+  address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
