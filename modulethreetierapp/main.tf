f77548d75801e9fdabd122f0c4e1e8323333d202
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 20:55:38 -0400

    Update main.tf

--- modulethreetierapp/main.tf
+++ modulethreetierapp/main.tf
@@ -134,8 +134,8 @@ resource "azurerm_linux_virtual_machine" "web_vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = var.admin_username_vm
-  admin_password      = var.admin_password_vm
+  admin_username_vm      = var.admin_username_vm
+  admin_password_vm      = var.admin_password_vm
   network_interface_ids = [
     azurerm_network_interface.web_nic.id,
   ]
