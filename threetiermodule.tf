b813bf9f4fc182a8fefe8b11b3a8efa17ebf4351
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 20:40:48 -0400

    Update threetiermodule.tf

--- threetiermodule.tf
+++ threetiermodule.tf
@@ -35,3 +35,17 @@ output "db_admin_password" {
   sensitive = true
   value = var.db_admin_password
 }
+variable "admin_password_vm"{
+  type=string
+}
+output "admin_password_vm" {
+  sensitive = true
+  value = var.admin_password_vm
+}
+variable "admin_password_vm"{
+  type=string
+}
+output "admin_username_vm" {
+  sensitive = true
+  value = var.admin_username_vm
+}
