b16c6d104acbc7c572ca05517f5ea00b0b0ef721
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 20:19:33 -0400

    Update threetiermodule.tf

--- threetiermodule.tf
+++ threetiermodule.tf
@@ -7,4 +7,31 @@ module "three_tier_app" {
   db_admin_username   = var.db_admin_username
   db_admin_password   = var.db_admin_password
 }
-
+variable "administrator_login"{
+  type=string
+}
+variable "administrator_login_password"{
+  type=string
+}
+variable "db_admin_username"{
+  type=string
+}
+variable "db_admin_password"{
+  type=string
+}
+output "admin_username" {
+  sensitive = true
+  value = var.administrator_login
+}
+output "admin_password" {
+  sensitive = true
+  value = var.administrator_login_password
+}
+output "db_admin_username" {
+  sensitive = true
+  value = var.db_admin_username
+}
+output "db_admin_password" {
+  sensitive = true
+  value = var.db_admin_password
+}
