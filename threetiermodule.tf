1d87f03c7eb609df3d66e137496c8243b045a50e
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 20:46:37 -0400

    Update threetiermodule.tf

--- threetiermodule.tf
+++ threetiermodule.tf
@@ -6,6 +6,8 @@ module "three_tier_app" {
   admin_password      = var.administrator_login_password
   db_admin_username   = var.db_admin_username
   db_admin_password   = var.db_admin_password
+  admin_username_vm   = var.admin_username_vm
+  admin_password _vm  = var. admin_username_vm
 }
 variable "administrator_login"{
   type=string
