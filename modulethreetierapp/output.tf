60d84956369d9c835d057a3216b5dc73f46ea8d1
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-27 20:15:00 -0400

    Update output.tf

--- modulethreetierapp/output.tf
+++ modulethreetierapp/output.tf
@@ -2,19 +2,4 @@ output "module_web_vm_public_ip" {
   description = "The public IP address of the web VM"
   value       = azurerm_linux_virtual_machine.web_vm.public_ip_address
 }
-output "admin_username" {
-  sensitive = true
-  value = var.administrator_login
-}
-output "admin_password" {
-  sensitive = true
-  value = var.administrator_login_password
-}
-output "db_admin_username" {
-  sensitive = true
-  value = var.db_admin_username
-}
-output "db_admin_password" {
-  sensitive = true
-  value = var.db_admin_password
-}
+
