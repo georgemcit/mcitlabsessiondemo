733c66ef812b064cc9248a062224840acda192d3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 19:05:52 -0400

    Update variable.tf

--- modulethreetierapp_count/variable.tf
+++ modulethreetierapp_count/variable.tf
@@ -1,3 +1,4 @@
+/*
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -32,3 +33,4 @@ variable "instance_count" {
   type        = number
   default     = 1
 }
+*/
