8bb346205cd8a527b33f1e654a009601b71ae324
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 19:10:00 -0400

    Update count_example.tf

--- count_example.tf
+++ count_example.tf
@@ -1,3 +1,8 @@
+
+resource "azurerm_resource_group" "georgeibrahim" {
+  name     = "george_ibrahim_1980_01_05"
+  location = "Canada Central"
+}
 variable "vm_count"{
     type=number
     default=4
@@ -9,13 +14,13 @@ variable "vm_size"{
 resource "azurerm_virtual_network" "vnetexample" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.georgeibrahim.name
   virtual_network_name = azurerm_virtual_network.vnetexample.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -23,8 +28,8 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_interface" "nic" {
   count               = var.vm_count
   name                = "example-nic-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
@@ -35,8 +40,8 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_linux_virtual_machine" "vm" {
   count               = var.vm_count
   name                = "example-vm-${count.index}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
   size                = var.vm_size
   admin_username      = var.administrator_login
   admin_password      = var.administrator_login_password
