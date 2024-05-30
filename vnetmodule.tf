34edeca42e0b3dec01aedf14587dddb56d6c625e
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 20:34:59 -0400

    Update vnetmodule.tf

--- vnetmodule.tf
+++ vnetmodule.tf
@@ -1,7 +1,11 @@
 
 module "module_vnet" {
-  source              = "./vnet/"
-  resource_group_name = "george_ibrahim"
-  location            = "West Europe"
+  source               = "./vnet/"
+  resource_group_name  = "george_ibrahim"
+  location             = "West Europe"
+  virtual_network_name = "george-network"
+  address_space        = ["10.0.0.0/16"]
+  subnet_name          = "george_sb"
+  address_prefixes     = ["10.0.2.0/24"]
 
 }
