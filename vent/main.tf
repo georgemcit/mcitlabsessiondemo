5e53efb008916cdfb6318992a24a003e8b2bd405
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 20:14:37 -0400

    Update main.tf

--- vent/main.tf
+++ vent/main.tf
@@ -1,27 +1,18 @@
-resource "azurerm_resource_group" "gi" {
-  name     = "george-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "georgeibrahim" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "george-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "georgevn" {
   name                = "george-network"
-  location            = azurerm_resource_group.gi.location
-  resource_group_name = azurerm_resource_group.gi.name
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  location            = azurerm_resource_group.georgeibrahim.location
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+}
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"   
-    security_group = azurerm_network_security_group.nsg.id
-  }
+resource "azurerm_subnet" "georgesb" {
+  name                 = "george_sb"
+  resource_group_name  = azurerm_resource_group.georgeibrahim.name
+  virtual_network_name = azurerm_virtual_network.georgevn.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
