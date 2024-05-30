2a83bcc60705a82984150fcdb331a44bb7a7108b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 20:57:50 -0400

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "georgeibrahim" {
 }
 
 resource "azurerm_virtual_network" "georgevn" {
-  name                = var.virtual_network_name
+  name                = var.vnet_name
   address_space       = var.address_space
   location            = azurerm_resource_group.georgeibrahim.location
   resource_group_name = azurerm_resource_group.georgeibrahim.name
