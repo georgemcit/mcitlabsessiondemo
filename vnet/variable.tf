91a80a6996245791f2e3ad7e0a57e07c3e224d13
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 20:38:01 -0400

    Update variable.tf

--- vnet/variable.tf
+++ vnet/variable.tf
@@ -7,3 +7,21 @@ variable "location" {
   description = "The Azure region to deploy the resources"
   type        = string
 }
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space ip"
+  type        = list
+}
+variable "subnet_name" {
+  description = "The name of the subnet name"
+  type        = string
+}
+
+variable "address_prefixes" {
+  description = "The address prefixes ip"
+  type        = list
+}
