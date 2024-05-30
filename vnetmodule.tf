0a19e1155b2dd53710ac2ee5d75887a5bb235899
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-29 20:27:45 -0400

    Update vnetmodule.tf

--- vnetmodule.tf
+++ vnetmodule.tf
@@ -2,6 +2,6 @@
 module "module_vnet" {
   source              = "./vnet/"
   resource_group_name = "george_ibrahim"
-  location                     = "West Europe"
+  location            = "West Europe"
 
 }
