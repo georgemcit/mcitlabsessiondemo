c4b4bcca02e676791960daabd26fb665bbb1c0aa
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-31 17:50:40 -0400

    Update vnetmodule.tf

--- vnetmodule.tf
+++ vnetmodule.tf
@@ -1,4 +1,4 @@
-
+/*
 module "module_vnet" {
   source               = "./vnet/"
   resource_group_name  = "george_ibrahim"
@@ -9,4 +9,4 @@ module "module_vnet" {
   address_prefixes     = ["10.0.2.0/24"]
 
 }
-
+*/
